b02ce1c59e4d48ab7b4115a38ebf602821a3b1e0
Yunho Nam <101496294+ManDuGuk@users.noreply.github.com>
2025-02-27 18:16:49 +0900

    tolist로컬스토리지 저장

--- js/todolist.js
+++ js/todolist.js
@@ -1,6 +1,15 @@
 const todoForm = document.querySelector("#todoForm");
 const todoList = document.querySelector("#todoList");
 
+let toDos = [];
+const TODOS_KEY = "todos"
+
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+
+}
+
 todoForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const todoInput = todoForm.querySelector("input")
@@ -8,6 +17,10 @@ todoForm.addEventListener("submit", (event) => {
 
     if (!inputVal) return alert("값을 입력하세요")
     else {
+
+        toDos.push(inputVal);
+        saveToDos();
+
         addTodo(inputVal);
         todoInput.value = "";
     }
@@ -29,14 +42,26 @@ const addTodo = (value) => {
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "done";
     todoLi.appendChild(deleteBtn);
-    todoForm.appendChild(todoLi);
+    todoList.appendChild(todoLi);
 
     //이벤트 리스너 추가
     deleteBtn.addEventListener("click", (event) => {
         console.dir(event.target);
         event.target.closest("li").remove();
-
+        toDos = toDos.filter(item => item !== event.target.closest("li"))
+        saveToDos();
     })
+
 };
 
 
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log("savedToDos", savedToDos);
+
+if (savedToDos !== null) {
+    toDos = JSON.parse(savedToDos)
+    console.log("parsedToDos", toDos);
+    toDos.forEach(element => {
+        addTodo(element)
+    });
+}
